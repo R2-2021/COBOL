5faf96409ea396e13e01ae9f21864d22c1b3d797
R2-2021 <79740964+R2-2021@users.noreply.github.com>
2023-11-19 09:08:46 +0900

    Delete C.DIR02.bat

--- C.DIR02.bat
+++
@@ -1,8 +0,0 @@
-REM *** Kachin-PC dir (UTF8) 取得 ***
-chcp 65001
-REM C:\Users\All Users => C:\ProgramData
-REM dir C:\Users\All Users /s > DIR.txt
-REM dir C:\Users\koko /s > DIR.txt
-REM dir C:\Users /s > DIR.txt
-
-dir M:\kachin-PC /s > DIR2.txt
